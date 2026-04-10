852233a5657aa44bfd48c4dd6541a10351243cce
Chris Graf <cgraf@meta.com>
2026-04-10 15:40:48 -0500

    Use truecolor white for section headers
    Summary
    - switch from SGR 97 (bright white) to 24-bit truecolor
      `\033[38;2;255;255;255m` for maximum brightness on dark
      terminal backgrounds
    
    Testing
    - verified headers render brighter than SGR 97 in iTerm2/tmux

--- .config/dot/core.sh
+++ .config/dot/core.sh
@@ -22,7 +22,7 @@ if [[ -t 1 && -z "${NO_COLOR:-}" ]]; then
   _C_YELLOW=$'\033[33m'
   _C_BLUE=$'\033[34m'
   _C_CYAN=$'\033[36m'
-  _C_WHITE=$'\033[97m'
+  _C_WHITE=$'\033[38;2;255;255;255m'
 else
   _C_RESET="" _C_BOLD="" _C_DIM="" _C_GREEN="" _C_YELLOW="" _C_BLUE="" _C_CYAN="" _C_WHITE=""
 fi
