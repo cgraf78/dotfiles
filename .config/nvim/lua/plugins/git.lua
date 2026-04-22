8c0ad0b64382a8b53dd3d71227ea2e2cf01f3e45
Chris Graf <cgraf@meta.com>
2026-04-22 06:24:57 -0700

    Add `vim-fugitive` for hunk-level diff revert in nvim
    Summary
    - add tpope/vim-fugitive plugin to git.lua
    - keybindings: <leader>gs for git status, <leader>gd for
      vertical diff split
    - lazy-loaded on :Git, :Gdiffsplit, :Gvdiffsplit commands
    
    Testing
    - installed via :Lazy, opened :Gvdiffsplit, verified hunk
      revert with do persists after saving and closing

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "tpope/vim-fugitive",
+    cmd = { "Git", "Gdiffsplit", "Gvdiffsplit" },
+    keys = {
+      { "<leader>gs", "<cmd>Git<cr>", desc = "Git status" },
+      { "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Git diff split" },
+    },
+  },
+  {
     "lewis6991/gitsigns.nvim",
     event = "BufReadPost",
     config = function()
@@ -11,11 +19,33 @@ return {
 
           map("n", "]h", gs.next_hunk, vim.tbl_extend("force", opts, { desc = "Next hunk" }))
           map("n", "[h", gs.prev_hunk, vim.tbl_extend("force", opts, { desc = "Previous hunk" }))
-          map("n", "<leader>hp", gs.preview_hunk, vim.tbl_extend("force", opts, { desc = "Preview hunk" }))
-          map("n", "<leader>hr", gs.reset_hunk, vim.tbl_extend("force", opts, { desc = "Reset hunk" }))
-          map("n", "<leader>hs", gs.stage_hunk, vim.tbl_extend("force", opts, { desc = "Stage hunk" }))
-          map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, vim.tbl_extend("force", opts, { desc = "Blame line" }))
-          map("n", "<leader>hB", gs.toggle_current_line_blame, vim.tbl_extend("force", opts, { desc = "Toggle inline blame" }))
+          map(
+            "n",
+            "<leader>hp",
+            gs.preview_hunk,
+            vim.tbl_extend("force", opts, { desc = "Preview hunk" })
+          )
+          map(
+            "n",
+            "<leader>hr",
+            gs.reset_hunk,
+            vim.tbl_extend("force", opts, { desc = "Reset hunk" })
+          )
+          map(
+            "n",
+            "<leader>hs",
+            gs.stage_hunk,
+            vim.tbl_extend("force", opts, { desc = "Stage hunk" })
+          )
+          map("n", "<leader>hb", function()
+            gs.blame_line({ full = true })
+          end, vim.tbl_extend("force", opts, { desc = "Blame line" }))
+          map(
+            "n",
+            "<leader>hB",
+            gs.toggle_current_line_blame,
+            vim.tbl_extend("force", opts, { desc = "Toggle inline blame" })
+          )
         end,
       })
     end,
